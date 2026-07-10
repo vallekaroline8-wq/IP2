c44d4964cad83ecf49aa24388a3c86b21ef25208
OscarM2003 <cerratooscar15@gmail.com>
2026-07-10 11:34:20 -0600

    Tokens

--- backend/procedures/usuarios.py
+++ backend/procedures/usuarios.py
@@ -26,3 +26,29 @@ def login_usuario(username):
     conexion.close()
 
     return usuario
+
+
+def obtener_usuario_por_id(id_usuario):
+
+    conexion = get_connection()
+
+    cursor = conexion.cursor(dictionary=True)
+
+    sql = """
+        SELECT
+            id_usuario,
+            nombre,
+            usuario,
+            rol,
+            estado
+        FROM tbl_usuario
+        WHERE id_usuario = %s
+    """
+
+    cursor.execute(sql, (id_usuario,))
+    usuario = cursor.fetchone()
+
+    cursor.close()
+    conexion.close()
+
+    return usuario
